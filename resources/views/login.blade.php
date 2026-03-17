019bf25309f8ba8d12f6b2ab7a85058afcc2cf24
Dahyna-Martinez <dahyna.martinez@upr.edu>
2026-03-17 16:08:19 -0400

    Changed Boostrap CDN depedency into npm download with Vite calls

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,7 +5,7 @@
     <title>MAIKINE Portal</title>
 
     <!-- Bootstrap CDN, this is temporary until we figure out how to install it locally -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <style>
         body{
